559e228205b32ddda11af0f84c855915f456b4c2
Rajmanimp <rajmani.patel08@gmail.com>
2021-10-09 10:10:58 +0530

    Update BookingDataRepository.java

--- bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/repository/BookingDataRepository.java
+++ bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/repository/BookingDataRepository.java
@@ -11,7 +11,7 @@ import com.paypal.bfs.test.bookingserv.entity.BookingData;
 @Repository
 public interface BookingDataRepository extends JpaRepository<BookingData, Long> {
 
-	@Query(value = "SELECT * FROM booking_data u WHERE u.first_name = ?1 and u.last_name = ?2 and date_of_birth = ?3 and request_time > DATEADD('MINUTE',-1, NOW())", nativeQuery = true)
+	@Query(value = "SELECT * FROM booking_data u WHERE u.first_name = ?1 and u.last_name = ?2 and date_of_birth = ?3 and request_time > DATEADD('MINUTE',-5, NOW())", nativeQuery = true)
 	List<BookingData> findRecentBookingForUser(String firstName, String lastName, String DOB);
 
 }
